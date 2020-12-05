19e2c064cee2a7e18ebb0f2c6001c8ad69be5bc9
Tristan Brindle <t.c.brindle@gmail.com>
2020-12-05 20:01:25 +0000

    Add a lines flow source
    Basically a wrapper around std::getline. Very useful for these
    puzzles, although returning a ref to the internal string makes
    me very uncomfortable...

--- common.hpp
+++ common.hpp
@@ -30,6 +30,39 @@ std::vector<T> vector_from_file(const char* path)
     return flow::from_istream<T>(str).to_vector();
 }
 
+namespace detail {
+
+template <typename CharT, typename Traits>
+struct lines_flow: flow::flow_base<lines_flow<CharT, Traits>> {
+private:
+    using istream_type = std::basic_istream<CharT, Traits>;
+    using string_type = std::basic_string<CharT, Traits>;
+    istream_type* is_;
+    string_type buf_;
+
+public:
+    explicit lines_flow(istream_type& is)
+        : is_(std::addressof(is))
+    {}
+
+    auto next() -> flow::maybe<string_type const&>
+    {
+        if (std::getline(*is_, buf_)) {
+            return {std::as_const(buf_)};
+        }
+        return {};
+    }
+};
+
+}
+
+template <typename CharT, typename Traits>
+detail::lines_flow<CharT, Traits>
+lines(std::basic_istream<CharT, Traits>& is)
+{
+    return detail::lines_flow<CharT, Traits>(is);
+}
+
 } // namespace aoc
 
 #endif
