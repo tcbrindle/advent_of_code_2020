90089ee4a8431a1d6a921b427172457e88a53f9d
Tristan Brindle <t.c.brindle@gmail.com>
2020-12-05 20:03:21 +0000

    Use bit-twiddling rather than binary search to find the seat no
    Makes things a bit nicer looking.

--- dec5/main.cpp
+++ dec5/main.cpp
@@ -1,33 +1,29 @@
 
 #include "../common.hpp"
 
-struct bounds { int lo, hi; };
-
-constexpr auto get_row = [](std::string_view str) -> int
-{
-    return flow::take(str, 7).fold([](bounds b,  char c) {
-        switch (c) {
-            case 'F': b.hi = (b.lo + b.hi)/2; break;
-            case 'B': b.lo = 1 + (b.lo + b.hi)/2; break;
-        }
-        return b;
-    }, bounds{0, 127}).lo;
-};
-
-constexpr auto get_column = [](std::string_view str) -> int
-{
-    return flow::drop(str, 7).fold([](bounds b,  char c) {
-        switch (c) {
-            case 'L': b.hi = (b.lo + b.hi)/2; break;
-            case 'R': b.lo = 1 + (b.lo + b.hi)/2; break;
-        }
-        return b;
-    }, bounds{0, 7}).lo;
-};
-
 constexpr auto get_seat_id = [](std::string_view str) -> int
 {
-    return 8 * get_row(str) + get_column(str);
+    return flow::fold(str, [](unsigned val,  char c) {
+        return (val << 1) + (c == 'B' || c == 'R');
+    }, 0u);
+};
+
+// To calculate part 2 we need the min, the max and the sum.
+// We could easily do this in two passes with minmax() and sum(),
+// but instead lets collect them all in one go using another fold.
+constexpr auto get_min_max_sum = [](auto&& input)
+{
+    struct stats {
+        int min = INT_MAX, max, sum;
+    };
+
+    return flow::fold(FLOW_FWD(input), [](stats s, int item) {
+        return stats {
+            .min = std::min(item, s.min),
+            .max = std::max(item, s.max),
+            .sum = s.sum + item
+        };
+    }, stats{});
 };
 
 constexpr std::pair<std::string_view, int> test_data[] = {
@@ -47,21 +43,10 @@ int main(int argc, char** argv)
         return -1;
     }
 
-    const auto vec = aoc::vector_from_file<std::string>(argv[1]);
+    std::ifstream input(argv[1]);
 
-    // To calculate part 2 we need the min, the max and the sum.
-    // We could easily do this in two passes with minmax() and sum(),
-    // but instead lets collect them all in one go using another fold.
-    struct stats { int min = INT_MAX, max, sum; };
-
-    const auto [min, max, sum] = flow::map(vec, get_seat_id).fold(
-        [](stats s, int item) {
-            return stats {
-                .min = std::min(item, s.min),
-                .max = std::max(item, s.max),
-                .sum = s.sum + item
-            };
-        }, stats{});
+    const auto [min, max, sum] =
+        aoc::lines(input).map(get_seat_id).apply(get_min_max_sum);
 
     fmt::print("Part 1: max seat id is {}\n", max);
 
