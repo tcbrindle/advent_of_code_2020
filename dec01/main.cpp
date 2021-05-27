f91e83ae5239ed4338aaedef37fe1cd494e440a9
Tristan Brindle <t.c.brindle@gmail.com>
2021-05-27 01:31:01 +0100

    Day 1: use our fancy new flow::cartestian_product
    ...just because we can

--- dec01/main.cpp
+++ dec01/main.cpp
@@ -1,9 +1,33 @@
 
 #include "../common.hpp"
 
+namespace {
+
+constexpr auto tuple_sum = [](auto&& tuple) {
+    return std::apply([](auto&&... args) { return (FLOW_FWD(args) + ...); }, FLOW_FWD(tuple));
+};
+
+constexpr auto tuple_product = [](auto&& tuple) {
+    return std::apply([](auto&&... args) { return (FLOW_FWD(args) * ...); }, FLOW_FWD(tuple));
+};
+
+constexpr auto find_value = [](auto&&... args) {
+    return flow::cartesian_product(FLOW_FWD(args)...)
+            .filter([](auto const& t) { return tuple_sum(t) == 2020; })
+            .map(tuple_product)
+            .next().value();
+};
+
+constexpr auto test_data = std::array{1721, 979, 366, 299, 675, 1456};
+
+static_assert(find_value(test_data, test_data)== 514579);
+
+static_assert(find_value(test_data, test_data, test_data) == 241861950);
+
+}
+
 int main(int argc, char** argv)
 {
-    // Read the input into a vector
     if (argc < 2) {
         fmt::print(stderr, "Error, no input\n");
         return -1;
@@ -11,32 +35,7 @@ int main(int argc, char** argv)
 
     const auto input = aoc::vector_from_file<int>(argv[1]);
 
-    // Just brute force things
-    const auto part1 = [&] {
-        for (int i : input) {
-            for (int j : input) {
-                if (i + j == 2020) {
-                    return i * j;
-                }
-            }
-        }
-        return -1;
-    }();
+    fmt::print("Part one: product is {}\n", find_value(input, input));
 
-    fmt::print("Part one: product is {}\n", part1);
-
-    const auto part2 = [&] {
-        for (int i : input) {
-            for (int j : input) {
-                for (int k : input) {
-                    if (i + j + k == 2020) {
-                        return i * j * k;
-                    }
-                }
-            }
-        }
-        return -1;
-    }();
-
-    fmt::print("Part two: product is {}\n", part2);
+    fmt::print("Part two: product is {}\n", find_value(input, input, input));
 }
